4cee6046331ad2f39a4acd9ce9b6047868330805
chapuisk <kevin.chapuis@gmail.com>
2018-01-26 19:40:19 +0700

    sphinc doc

--- test/testJasssScrap.py
+++ test/testJasssScrap.py
@@ -9,5 +9,5 @@ title = jasssScrap.get_title(soup)
 key_worlds = jasssScrap.get_key_worlds(soup)
 authors = jasssScrap.get_authors(soup)
 
-print("The first article published article in JASSS was: " + title + "\n" + "The key words was: " + str(
+print("The first published article in JASSS was: " + title + "\n" + "The key words was: " + str(
     key_worlds) + "\n" + "And authors was: " + str(authors))
