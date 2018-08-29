84ad4d9f1de158f342c77cb75b5791fe703039cb
anthonyfouchard <anthony.fouchard.rt@gmail.com>
2018-08-29 11:42:33 +0700

    scraping + NLP

--- spiders/nltk_test.py
+++ spiders/nltk_test.py
@@ -1,19 +1,26 @@
+import finder as finder
+import ngram as ngram
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-#nltk.download('all')
+import numpy as np
+from nltk.util import ngrams
+# nltk.download('all')
 
 class Nltk():
     def __init__(self):
+
         f = open('article.txt', 'rU')
         self.content = f.read()
+        f.close()
         fullf = open('fullarticle.txt', 'rU')
         self.fullcontent = fullf.read()
         self.stopWords = set(stopwords.words('english'))
         # stemming
         self.lemma = nltk.wordnet.WordNetLemmatizer()
         self.stemmer = nltk.SnowballStemmer("english")
-    def Freqwordss(self):
+
+    def Nlp_article(self):
 
         tokens = nltk.word_tokenize(self.content)
         tagged = nltk.pos_tag(tokens)
@@ -21,22 +28,36 @@ class Nltk():
 
         filtered_word = nltk.word_tokenize(self.content)
         # Remove single-character tokens (mostly punctuation)
-        filtered_word = [word for word in filtered_word if len(word) > 3]
+        filtered_word = [word for word in filtered_word if len(word) > 1]
         # Remove numbers
         filtered_word = [word for word in filtered_word if not word.isnumeric()]
         # Remove basic english words
         filtered_word = [word for word in filtered_word if word not in self.stopWords]
-        #remove majuscule
+        # remove majuscule
         filtered_word = [word.lower() for word in filtered_word]
-
+        filtered_word = [self.lemma.lemmatize(word) for word in filtered_word]
         wnl = WordNetLemmatizer()
         fdist = nltk.FreqDist(filtered_word)
 
-        for word, frequency in fdist.most_common(50):
-            print(u'{};{};{};{}'.format(word, wnl.lemmatize(word), self.stemmer.stem(word), self.lemma.lemmatize(word), frequency))
+        vocab_to_index = {word: i for i, word in enumerate(filtered_word)}
+        finder = list(nltk.bigrams(filtered_word))
+        bigram_freq = nltk.FreqDist(finder).most_common(len(finder))
+        co_occurrence_matrix = np.zeros((len(filtered_word), len(filtered_word)))
+
+        for bigram in bigram_freq:
+            current = bigram[0][1]
+            previous = bigram[0][0]
+            count = bigram[1]
+
+            pos_current = vocab_to_index[current]
+            pos_previous = vocab_to_index[previous]
+            co_occurrence_matrix[pos_current][pos_previous] = count
+
+        co_occurrence_matrix = np.matrix(co_occurrence_matrix)
+        print(co_occurrence_matrix)
 
     def corpus(self, spliter):
-        matrix=[]
+        matrix = []
         filtered_word = self.fullcontent.split(spliter)
 
         filtered_word = [word.lower() for word in filtered_word]
@@ -51,7 +72,7 @@ class Nltk():
         print(matrix)
 
     def split_ngram(self, spliter):
-        matrix=[]
+        matrix = []
         filtered_word = self.content.split(spliter)
 
         filtered_word = [word.lower() for word in filtered_word]
@@ -65,7 +86,9 @@ class Nltk():
 
         print(matrix)
 
+
 if __name__ == "__main__":
     process = Nltk()
-    process.split_ngram(spliter=str("."))
-    process.corpus(spliter=str("."))
+    # process.split_ngram(spliter=str("."))
+    # process.corpus(spliter=str("."))
+    process.Nlp_article()
--- spiders/scrapapi.py
+++ spiders/scrapapi.py
@@ -1,4 +1,10 @@
+# -*- coding: utf-8 -*-
 import requests
+from elsapy.elsclient import ElsClient
+from elsapy.elsprofile import ElsAuthor, ElsAffil
+from elsapy.elsdoc import FullDoc, AbsDoc
+from elsapy.elssearch import ElsSearch
+import json
 
 """
 example requests : http://api.plos.org/search?q=title:"Drosophila" AND body:"RNA"&fl=id,abstract
@@ -16,8 +22,8 @@ class ScrapingApi():
             ScrapingApi.plosone()
 
         if api == "sciencedirect":
-            self.url =
-            ScrapingApi.science_direct()
+            self.url = "https://api.elsevier.com/authenticate/"
+            ScrapingApi.science_direct(self)
 
     def plosone(self):
         if self.feature=="deep":
@@ -34,6 +40,33 @@ class ScrapingApi():
                 print(resp.text)
 
     def science_direct(self):
-        
+        payload={'APIKey': "9b6fc59147caceaff0944b9259dec395"}
+        response=requests.get("http://api.elsevier.com/content/search/scopus?query=KEY%28agent-based%29", payload)
 
-ScrapingApi("plosone", 'agent-based', "deep")
+        ## Load configuration
+        con_file = open("config.json")
+        config = json.load(con_file)
+        con_file.close()
+
+        ## Initialize client
+        client = ElsClient(config['apikey'])
+
+        ## ScienceDirect (full-text) document example using PII
+        pii_doc = FullDoc(sd_pii='S1674927814000082')
+        if pii_doc.read(client):
+            print("pii_doc.title: ", pii_doc.data)
+            pii_doc.write()
+        else:
+            print("Read document failed.")
+
+        ## ScienceDirect (full-text) document example using DOI
+        doi_doc = FullDoc(doi='10.1016/S1525-1578(10)60571-5')
+        if doi_doc.read(client):
+            print("doi_doc.title: ", doi_doc.data)
+            doi_doc.write()
+        else:
+            print("Read document failed.")
+
+
+
+ScrapingApi("sciencedirect", 'agent-based', "deep")
