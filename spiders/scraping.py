66afa15fa6eadf3ac2ba6cb1f0d7cd9d23857196
anthonyfouchard <anthony.fouchard.rt@gmail.com>
2018-10-24 16:27:31 +0200

    Update scraping.py

--- spiders/scraping.py
+++ spiders/scraping.py
@@ -14,9 +14,12 @@ from scrapy.selector import HtmlXPathSelector, Selector
 #import pywin32
 
 class WebScraping(scrapy.Spider):
+    #name of our spider
     name = "scraping"
+    #list of url we want to scrap
     start_urls = ['http://jasss.soc.surrey.ac.uk/index_by_issue.html', 'https://www.comses.net/codebases']
-
+    
+    #rule to extract element in urls, allow to extract "page=xxx"
     le1 = LinkExtractor(canonicalize=True, unique=False)
     rules = [
         Rule(
@@ -25,7 +28,9 @@ class WebScraping(scrapy.Spider):
             callback="parse_items"
         )
     ]
+    
     list_urls = []
+    #define if we want to search with keyword and article content
     def __init__(self, keyword, search):
         self.keyword = keyword
         self.search = search
@@ -34,15 +39,19 @@ class WebScraping(scrapy.Spider):
     def start_requests(self):
         print(self.start_urls)
         self.items = dict()
+        #if we want to choose which url we want to scrap, in this case we define directly url 0 (jasss)
         #url = str(input("Index, enter n° : "))
         url=0
+        #define start_urls as url we want
         self.start_urls = self.start_urls[int(url)]
 
         """Give Domain with URL"""
+        #parse start url to have our domain
         parsed_uri = urlparse(self.start_urls)
         self.domain = parsed_uri.netloc
         print("domain : ",self.domain)
-
+        
+        #return request
         yield scrapy.Request(url=self.start_urls, callback=self.parse)
 
     # the response containing a HTML form
@@ -50,28 +59,33 @@ class WebScraping(scrapy.Spider):
         # extract data from every links
         links = self.le1.extract_links(response)
 
-        #deep search
+        #in case of deep search
         if self.search == "deep":
             """Body of article content"""
             for link in links:
                 print(link.url)
+                #add url in empty list we created before
                 WebScraping.list_urls.append(str(link.url))
-
+            #launch class scraping_content after have list of urls list
             process.crawl(scraping_content)
 
-        #basic search
+        #in case of basic search
         else:
+            #open file to put content inside
             file = open('content.txt', 'w')
             for link in links:
                 #print(link.url, link.text)
                 """ match URL with title and put them in a dict"""
                 if self.keyword in link.text.lower():
+                    #associate url to article title
                     self.items['url'] = str(link.title)
                     file.write(str(self.items['url']))
 
             file.close()
-
+            
+#after we created list of urls list we can scraping every link
 class scraping_content(scrapy.Spider):
+    #name of second spider
     name = "scraping_content"
     start_urls = WebScraping.list_urls
     items = []
@@ -84,12 +98,13 @@ class scraping_content(scrapy.Spider):
         )
     ]
     def start_requests(self):
-
+        #content scraping on each url
         for url in self.start_urls:
             yield scrapy.Request(url=url, callback=self.parsing)
-
+        #open file to put data inside
         file = open('content.txt', 'w')
-
+        
+        #this part doesn't work, try to delete character we don't want but too many cases
         for i in self.items:
             i = i.replace(u'\xa0', u' ')
             i = i.replace(u'\r', u'')
@@ -97,14 +112,20 @@ class scraping_content(scrapy.Spider):
             #encode ou decode?
             file.write(str(i.encode('utf-8')))
         file.close()
+        
+        
     def parsing(self, response):
-
+        #different responses depends on website we want to analyze
+        
         #self.item['keyword'] = str(response.xpath('//div[@id="keywords"]/a/text()').extract())
         #self.item['content'] = str(response.xpath('//body//p/text()').extract())
+        
+        #this one works on jasss article
         self.items.append(str(response.xpath('//body//p/text()').extract()))
 
 #Launch spider
 if __name__ == "__main__":
+    #launch spider directly in script (usefull for windows environment)
     process = CrawlerProcess()
     """keyword to do a keyword search,search=deep to a deep search"""
     process.crawl(WebScraping, keyword="", search="deep")
@@ -112,6 +133,7 @@ if __name__ == "__main__":
 
 
 # follow next page
+#we have to include this part in script if we have index with many pages, it works for OpenABM
 """
 try:
     #this response use css, only for comses.net /// Change page-item to adapt
